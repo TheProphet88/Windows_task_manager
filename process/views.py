43f55d2f9a46ab15b21c2be373b4d9c62c4e211b
როსტომ შერაზადიშვილი <90464366+TheProphet88@users.noreply.github.com>
2022-11-09 13:38:57 +0400

    first commit

--- process/views.py
+++ process/views.py
@@ -6,78 +6,34 @@ import psutil
 def process_monitor(request):
     """ ფუნქცია ფრონტიდან, მიღებული რექსვესტის შემდეგ, ამოწმებს ოპერაციულ სისტემას და 
     ამის მიხედვით ადგენს, რომელი კონდიცია გაუშვას. """
-    
-    if psutil.LINUX:
-        # ეს ნაწილი უნდა გააქტიურდეს თუ კომპიუტერის ოპერაციული სისტემა არის ლინუქსი.
-        
-        if request.method == 'POST':
-            stop_process = request.POST.get('stop')
-            resume_process = request.POST.get('resume')
-            terminate_process = request.POST.get('terminate')
-            # აქედან მოწმდება ფრონტიდან რომელ ღილაკს დაეჭირა. 
-            # და ღილაკზე დაჭერის მიხედვით აქტიურდება, შესაბამისი ოპერაცია სერვისზე.
-            
-            try:    
-                # შეაჩერე პროცესი.
-                if stop_process:
-                    stop1 = int(stop_process)
-                    stop2 = psutil.Process(pid=stop1)
-                    stop2.suspend()
-                    return redirect('/')
 
-                # განაახლე პროცესი.    
-                if resume_process:
-                    rezum1 = int(resume_process)
-                    rezum2 = psutil.Process(pid=rezum1)
-                    rezum2.resume()
-                    return redirect('/')
-            
-                # გააუქმე პროცესი.
-                if terminate_process:
-                    terminate = int(terminate_process)
-                    terminate2 = psutil.Process(pid=terminate)
-                    terminate2.terminate()
-                    return redirect('/')
+    stop_process = request.POST.get('stop')
+    resume_process = request.POST.get('resume')
+    terminate_process = request.POST.get('terminate')
+    try:       
+        # შეაჩერე პროცესი.
+        if stop_process:
+            stop1 = int(stop_process)
+            stop2 = psutil.Process(pid=stop1)
+            stop2.suspend()
+            return redirect('/')
                 
-            except psutil.NoSuchProcess:
-                pass
-        
-        context = {'detail': psutil.process_iter(),
-               #'serviz': func(),
-               'cpu_percent': psutil.cpu_percent(),
-               'memory_percent': psutil.virtual_memory(),}
-        
-        return render(request, 'process/processess.html', context)          
-    
-    # ვინდოუსი.
-    else:
-        stop_process = request.POST.get('stop')
-        resume_process = request.POST.get('resume')
-        terminate_process = request.POST.get('terminate')
-        try:       
-            # შეაჩერე პროცესი.
-            if stop_process:
-                stop1 = int(stop_process)
-                stop2 = psutil.Process(pid=stop1)
-                stop2.suspend()
-                return redirect('/')
-                
-            # განაახლე პროცესი.
-            if resume_process:
-                rezum1 = int(resume_process)
-                rezum2 = psutil.Process(pid=rezum1)
-                rezum2.resume()
-                return redirect('/')
+        # განაახლე პროცესი.
+        if resume_process:
+            rezum1 = int(resume_process)
+            rezum2 = psutil.Process(pid=rezum1)
+            rezum2.resume()
+            return redirect('/')
 
-            # გააუქმე პროცესი.    
-            if terminate_process:
-                terminate = int(terminate_process)
-                terminate2 = psutil.Process(pid=terminate)
-                terminate2.terminate()
-                return redirect('/')
+        # გააუქმე პროცესი.    
+        if terminate_process:
+            terminate = int(terminate_process)
+            terminate2 = psutil.Process(pid=terminate)
+            terminate2.terminate()
+            return redirect('/')
         
-        except psutil.NoSuchProcess:
-                pass
+    except psutil.NoSuchProcess:
+            pass
 
     context1 = {'detail': psutil.process_iter(),
                'service': psutil.win_service_iter(),
